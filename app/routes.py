cb702916ef1cbf56cc0136b2a50c65e79b6ac5f5
Nikita Cherenkov <17rolex@gmail.com>
2021-04-13 14:09:09 +0300

    add edit_profile

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app import app, db
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post, Profile
@@ -64,7 +64,7 @@ def register():
     # submit validation
 
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        user = User(username=form.username.data.strip().title(), email=form.email.data.strip().lower())
         user.set_password(form.password.data)
 
         db.session.add(user)
@@ -86,6 +86,42 @@ def logout():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    profile = Profile.query.filter_by(user_id=user.id).all()
-    posts = Post.query.filter_by(user_id=user.id).all()
+    profile = user.profile.first()
+    posts = user.posts.all()
     return render_template('profile.html', user=user, profile=profile, posts=posts)
+
+
+@app.route('/user/<username>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_profile(username):
+    
+    form = EditProfileForm()
+
+    if current_user.profile.first():
+        profile = current_user.profile.first()
+        if form.validate_on_submit():
+            profile.update_info(gender=form.gender.data,
+                                info=form.info.data,
+                                photo='1',
+                                date_of_birth=form.date_of_birth.data)
+
+            db.session.add(profile)
+            db.session.commit()
+
+            flash('Saved')
+            return redirect(url_for('user', username=current_user.username))
+    else:
+        profile = Profile(user_id=current_user.id,
+                          gender=form.gender.data,
+                          info=form.info.data,
+                          date_of_birth=form.date_of_birth.data)
+        if form.validate_on_submit():
+
+            db.session.add(profile)
+            db.session.commit()
+
+            flash('Saved')
+            return redirect(url_for('user', username=current_user.username))
+
+
+    return render_template('edit_profile.html', user=current_user, profile=profile, form=form)
